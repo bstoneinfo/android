c9235fcf8b4df237b8c353b7e806a07bfdf058da
xiaodongzhao <bstoneinfo@163.com>
2014-08-23 09:35:00 +0800

    创建项目BeautyMM

---
+++ fashion/beautymm/src/custom/R.java
@@ -0,0 +1,13 @@
+package custom;
+
+public final class R {
+    public static final com.bstoneinfo.beauty.mm.R.attr attr = new com.bstoneinfo.beauty.mm.R.attr();
+    public static final com.bstoneinfo.beauty.mm.R.color color = new com.bstoneinfo.beauty.mm.R.color();
+    public static final com.bstoneinfo.beauty.mm.R.drawable drawable = new com.bstoneinfo.beauty.mm.R.drawable();
+    public static final com.bstoneinfo.beauty.mm.R.id id = new com.bstoneinfo.beauty.mm.R.id();
+    public static final com.bstoneinfo.beauty.mm.R.layout layout = new com.bstoneinfo.beauty.mm.R.layout();
+    public static final com.bstoneinfo.beauty.mm.R.string string = new com.bstoneinfo.beauty.mm.R.string();
+    public static final com.bstoneinfo.beauty.mm.R.style style = new com.bstoneinfo.beauty.mm.R.style();
+    public static final com.bstoneinfo.beauty.mm.R.styleable styleable = new com.bstoneinfo.beauty.mm.R.styleable();
+    public static final com.bstoneinfo.beauty.mm.BuildConfig buildConfig = new com.bstoneinfo.beauty.mm.BuildConfig();
+}
